1b9fc16578c7b5e062444b17b5c0aa9f08b7bfe1
Ravil Berishev <quadraticteam@gmail.com>
2022-02-01 20:41:24 +0300

    check null isObjectUpdated

--- src/engine/modules/isObjectUpdated.ts
+++ src/engine/modules/isObjectUpdated.ts
@@ -1,6 +1,9 @@
 import LINQ from '@berish/linq';
 
 export function isObjectUpdated(prevObject: Record<any, any>, newObject: Record<any, any>) {
+  prevObject = prevObject || {};
+  newObject = newObject || {};
+
   return LINQ.from(Object.keys(prevObject).concat(Object.keys(newObject)))
     .distinct()
     .some((key) => !Object.is(prevObject[key], newObject[key]));
